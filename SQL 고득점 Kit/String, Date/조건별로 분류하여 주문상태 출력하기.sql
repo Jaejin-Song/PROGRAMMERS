461236a379e4080b765c62f729f4c91896f449e4
Jaejin Song <songjj77@naver.com>
2023-08-12 16:51:19 +0900

    String, Date : 조건별로 분류하여 주문상태 출력하기

---
+++ SQL 고득점 Kit/String, Date/조건별로 분류하여 주문상태 출력하기.sql
@@ -0,0 +1,8 @@
+SELECT order_id, product_id, DATE_FORMAT(out_date, '%Y-%m-%d'),
+    CASE 
+        WHEN DATEDIFF('2022-05-01', out_date) >= 0 THEN '출고완료'
+        WHEN out_date IS NULL THEN '출고미정'
+        ELSE '출고대기'
+    END AS '출고여부'
+    FROM food_order
+    ORDER BY order_id ASC;
